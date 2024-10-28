774c2ec271a9c2b950be17202bd032a8fdc0d80a
Juni <103315958+Juniteevee@users.noreply.github.com>
2024-10-28 07:36:27 +0000

    fix: error on text-only post (#19)

--- pkgs/app/src/lib/parseEmbedDescription.ts
+++ pkgs/app/src/lib/parseEmbedDescription.ts
@@ -7,8 +7,11 @@ export function parseEmbedDescription(post: AppBskyFeedDefs.PostView): string {
     (checkType('app.bsky.embed.record#view', post.embed) ||
       checkType('app.bsky.embed.recordWithMedia#view', post.embed));
 
-  // @ts-expect-error
-  const embed = post.embed.record?.record ?? post.embed.record;
+  let embed;
+  if (isQuote) {
+    // @ts-expect-error
+    embed = post.embed.record?.record ?? post.embed.record;
+  }
 
   return isQuote
     ? // @ts-expect-error
