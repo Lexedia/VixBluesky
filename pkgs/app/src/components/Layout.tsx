37280716ff389847d8f410edcc7258e550dccf63
Timothy Cyrus <4201229+tcyrus@users.noreply.github.com>
2026-02-03 02:17:22 -0500

    Fix canonical URL + add og:url (#36)
    Co-authored-by: Lexedia <contact@lexedia.moe>

--- pkgs/app/src/components/Layout.tsx
+++ pkgs/app/src/components/Layout.tsx
@@ -14,11 +14,11 @@ export const Layout = ({ url, children }: LayoutProps) => {
     <!doctype html>
     <html>
       <head>
-        <link rel="canonical" href="${url.substring(1)}" />
+        <link rel="canonical" href="${redirectUrl}"/>
         <meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
-        <meta content="#0085ff" name="theme-color" />
-        <meta property="og:site_name" content="VixBluesky" />
-
+        <meta property="og:url" content="${redirectUrl}"/>
+        <meta name="theme-color" content="#0085ff"/>
+        <meta property="og:site_name" content="VixBluesky"/>
         ${children}
         <meta http-equiv="refresh" content="0;url=${redirectUrl}" />
       </head>
